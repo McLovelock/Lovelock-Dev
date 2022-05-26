b5ea6e0850769ea6045c91281633f673c2981417
59365069 <matija.brown@outlook.de>
2022-05-26 15:32:30 +0200

    Lovelock Mod Initial

---
+++ src/main/java/io/github/mclovelock/lovelock/Lovelock.java
@@ -0,0 +1,10 @@
+package io.github.mclovelock.lovelock;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Lovelock.MODID)
+public class Lovelock {
+
+	public static final String MODID = "lovelock";
+	
+}
